2c77ea56ca0afb1566493f36ccc3ec24d0cf4be7
JayN <quy.nguyen@codemode.com.au>
2025-01-14 16:45:02 +0700

    Update

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.test.ts", "**/__tests__/**/*.test.js"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-    "^.+\\.(js|jsx)$": "babel-jest",
-  },
-  transformIgnorePatterns: [
-    "/node_modules/(?!node-fetch|data-uri-to-buffer|fetch-blob|formdata-polyfill)",
-  ],
+  testMatch: ["**/__tests__/**/*.test.ts"],
 };
